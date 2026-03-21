acea0bcfab986866336588669ae1e6ab2e92924f
Jan Olav Berg <boj012026@gmail.com>
2026-03-21 12:08:48 +0100

    input to object

---
+++ app.js
@@ -0,0 +1,14 @@
+const artSeed = [
+    {   shape: "square",
+	show: (document.getElementById("show-squares")),
+	amount: (document.getElemntByID("amount-squares"))
+    },
+    {	shape: "diamond",
+	show: (document.getElementById("show-diamond")),
+	amount: document.getElementById("amount-diamond")
+    }
+]
+
+//const artSeed = [];
+//artSeed.push({shape = "square";
+console.log(artSeed);
